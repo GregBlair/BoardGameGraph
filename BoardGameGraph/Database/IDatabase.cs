ecf349f681a2874310174084ef6f5a4d4ac71874
GregBlair <gregblair@msn.com>
2016-03-30 20:57:28 -0500

    Went to a MongoDB seminar last night and remembered how the dababase works.

---
+++ BoardGameGraph/Database/IDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGameGraph.Database
+{
+    struct PlayEntry
+    {
+        uint XDestination;
+        uint YDestination;
+        uint XStart;
+        uint YStart;
+        string NextEntry;
+        string Symmetry;
+    }
+
+    interface IDatabase
+    {
+        string createPlay(PlayEntry entry);
+        string createMove(PlayEntry entry);
+    }
+}
---
+++ BoardGameGraph/Database/MongoDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGameGraph.Database
+{
+    class MongoDB : IDatabase
+    {
+        public string createMove(PlayEntry entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string createPlay(PlayEntry entry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
